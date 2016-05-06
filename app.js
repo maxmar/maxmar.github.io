727393190323dfb2755d80f78fdf6a789aaf37aa
MaxMar <max.marchenko.o@gmail.com>
2016-05-06 23:11:46 +0300

    Comment unused log messages

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ Notification.requestPermission();
 window.addEventListener('load', function() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function(reg) {
-      if (reg.installing)
-        console.log('Service worker installing');
+      // if (reg.installing)
+      //   console.log('Service worker installing');
 
-      else if(reg.waiting)
-        console.log('Service worker installed');
+      // else if(reg.waiting)
+      //   console.log('Service worker installed');
 
-      else if(reg.active)
-        console.log('Service worker active');
+      // else if(reg.active)
+      //   console.log('Service worker active');
 
       initialiseState(reg);
     });  
@@ -22,68 +22,41 @@ window.addEventListener('load', function() {
 
 
 function initialiseState(reg) {
-  // Are Notifications supported in the service worker?
-  if (!(reg.showNotification)) {
-    console.log('Notifications aren\'t supported on service workers.');
+  if ( ! (reg.showNotification)) {
+    // console.log('Notifications aren\'t supported on service workers.');
     useNotifications = false;
 
   } else {
     useNotifications = true;
   }
 
-  // Check the current Notification permission.
-  // If its denied, it's a permanent block until the
-  // user changes the permission
   if (Notification.permission === 'denied') {
-    console.log('The user has blocked notifications.');
+    // console.log('The user has blocked notifications.');
     return;
   }
 
-  // Check if push messaging is supported
   if (!('PushManager' in window)) {
-    console.log('Push messaging isn\'t supported.');
+    // console.log('Push messaging isn\'t supported.');
     return;
   }
 
-  // We need the service worker registration to check for a subscription
   navigator.serviceWorker.ready.then(function(reg) {
-    // Do we already have a push message subscription?
     reg.pushManager.subscribe({ userVisibleOnly: true })
       .then(function(subscription) {
 
         if ( ! subscription) {
-          console.log('Not yet subscribed to Push')
-          // We aren't subscribed to push, so set UI
-          // to allow the user to enable push
+          // console.log('Not yet subscribed to Push')
           return;
         }
-        
-        // initialize status, which includes setting UI elements for subscribed status
-        // and updating Subscribers list via push
-        console.log(subscription.toJSON());
+
+        // console.log(subscription.toJSON());
         var endpoint = subscription.endpoint;
-        var key = subscription.getKey('p256dh');
-        console.log(key);
-
-
-        console.log(subscription.getKey('p256dh'));
-        console.log(subscription.getKey('auth'));
 
         // updateStatus(endpoint,key,'init');
       })  
       .catch(function(err) {
-        console.log('Error during getSubscription()', err);
+        console.log('Error during subscribe()', err);
       });
-
-      // set up a message channel to communicate with the SW
-      var channel = new MessageChannel();
-      channel.port1.onmessage = function(e) {
-        console.log(e);
-        handleChannelMessage(e.data);
-      }
-      
-      mySW = reg.active;
-      mySW.postMessage('hello', [channel.port2]);
   });  
 }
 
