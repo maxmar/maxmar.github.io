c603c0a599ebf8b361dec598f113eac4d1c39c08
MaxMar <max.marchenko.o@gmail.com>
2016-05-06 22:46:21 +0300

    Another version of this old code -_-

--- app.js
+++ app.js
@@ -1,22 +1,87 @@
-if ("serviceWorker" in navigator) {
-  swPromise = navigator.serviceWorker.register("/sw.js")
+Notification.requestPermission();
 
-  // swPromise.then(initialiseState);
-  Notification.requestPermission().then(function() {
-    swPromise.then(function(registration) {
-      return registration.pushManager.subscribe({ userVisibleOnly: true });
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+      if (reg.installing)
+        console.log('Service worker installing');
 
-    }).then(function(subscription) {
-      console.log(subscription);
+      else if(reg.waiting)
+        console.log('Service worker installed');
 
-    }).catch(function(err) {
-      console.log("There was a problem with the Service Worker");
-      console.log(err);
-    });
-  });
+      else if(reg.active)
+        console.log('Service worker active');
+
+      initialiseState(reg);
+    });  
+  } else {
+    console.log('Service workers aren\'t supported in this browser.');
+  }
+});
+
+
+
+function initialiseState(reg) {
+  // Are Notifications supported in the service worker?
+  if (!(reg.showNotification)) {
+    console.log('Notifications aren\'t supported on service workers.');
+    useNotifications = false;
+
+  } else {
+    useNotifications = true;
+  }
+
+  // Check the current Notification permission.
+  // If its denied, it's a permanent block until the
+  // user changes the permission
+  if (Notification.permission === 'denied') {
+    console.log('The user has blocked notifications.');
+    return;
+  }
+
+  // Check if push messaging is supported
+  if (!('PushManager' in window)) {
+    console.log('Push messaging isn\'t supported.');
+    return;
+  }
+
+  // We need the service worker registration to check for a subscription
+  navigator.serviceWorker.ready.then(function(reg) {
+    // Do we already have a push message subscription?
+    reg.pushManager.subscribe({ userVisibleOnly: true })
+      .then(function(subscription) {
+
+        if ( ! subscription) {
+          console.log('Not yet subscribed to Push')
+          // We aren't subscribed to push, so set UI
+          // to allow the user to enable push
+          return;
+        }
+        
+        // initialize status, which includes setting UI elements for subscribed status
+        // and updating Subscribers list via push
+        console.log(subscription.toJSON());
+        var endpoint = subscription.endpoint;
+        var key = subscription.getKey('p256dh');
+        console.log(key);
+
+        // updateStatus(endpoint,key,'init');
+      })  
+      .catch(function(err) {
+        console.log('Error during getSubscription()', err);
+      });
+
+      // set up a message channel to communicate with the SW
+      var channel = new MessageChannel();
+      channel.port1.onmessage = function(e) {
+        console.log(e);
+        handleChannelMessage(e.data);
+      }
+      
+      mySW = reg.active;
+      mySW.postMessage('hello', [channel.port2]);
+  });  
 }
-
-
 
 
 
