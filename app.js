78a3ebd6ae1437f6ec9860e7f4e47518b4049f32
MaxMar <max.marchenko.o@gmail.com>
2016-05-06 21:55:10 +0300

    Add promise for permission request

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ if ("serviceWorker" in navigator) {
   swPromise = navigator.serviceWorker.register("/sw.js")
 
   // swPromise.then(initialiseState);
+  Notification.requestPermission().then(function() {
+    swPromise.then(function(registration) {
+      return registration.pushManager.subscribe({ userVisibleOnly: true });
 
-  swPromise.then(function(registration) {
-    return registration.pushManager.subscribe({ userVisibleOnly: true });
+    }).then(function(subscription) {
+      console.log(subscription);
 
-  }).then(function(subscription) {
-    console.log(subscription);
-
-  }).catch(function(err) {
-    console.log("There was a problem with the Service Worker");
-    console.log(err);
+    }).catch(function(err) {
+      console.log("There was a problem with the Service Worker");
+      console.log(err);
+    });
   });
 }
 
