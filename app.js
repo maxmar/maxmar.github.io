9164e0f97ef4fbd59e139dd33442eafe6e0277dc
MaxMar <max.marchenko.o@gmail.com>
2016-05-06 21:19:51 +0300

    Remove unused code

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-Notification.requestPermission();
-
-if ("serviceWorker" in navigator) {
-  swPromise = navigator.serviceWorker.register("/sw.js")
-
-  swPromise.then(function(registration) {
-        return registration.pushManager.subscribe({ userVisibleOnly: true });
-
-    }).then(function(subscription) {
-          console.log(subscription);
-
-    }).catch(function(err) {
-          console.log("There was a problem with the Service Worker");
-          console.log(err);
-    });
-}
-
-
-
-
-
 var isPushEnabled = false;
 
 window.addEventListener('load', function() {
