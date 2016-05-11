b3274ca4a8069af8f73c6a0c25bfa4cc1dcbfbd5
MaxMar <max.marchenko.o@gmail.com>
2016-05-11 20:26:30 +0300

    Remove private data from php call

--- push.php
+++ push.php
@@ -1,6 +1,6 @@
 <?php
-$authKey = 'AIzaSyAW3ew5SBdyffNzXDO-cAyKH0vrf52h1o0';
-$endpoints[] = 'cTDfCznf8QA:APA91bGsmB6bPl4MQncm0RxYt39SboIiK6k4C_GbZ7ZfO6AJmXq62sfYuTiYdIUZa0RVXFSAsSiLP1eabn-uB2tHmof68Z5_bMd8UExfAgTW8jbBmMUyb7tgOjB7CSX2Wk06kuTw6APz';
+$authKey = '<YOUR_API_KEY';
+$endpoints[] = '<YOUR_TEST_ENDPOINT';
 $gcmUrl = 'https://android.googleapis.com/gcm/send';
 
 $fields = [
