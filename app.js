679936aa220f53d7f2f26b2da2fb422044e6cd1b
MaxMar <max.marchenko.o@gmail.com>
2016-08-04 10:47:09 +0300

    updates

--- app.js
+++ app.js
@@ -1,80 +1,147 @@
+// Notification.requestPermission();
+
+// window.addEventListener('load', function() {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/sw.js').then(function(reg) {
+//       // if (reg.installing)
+//       //   console.log('Service worker installing');
+
+//       // else if(reg.waiting)
+//       //   console.log('Service worker installed');
+
+//       // else if(reg.active)
+//       //   console.log('Service worker active');
+
+//       initialiseState(reg);
+//     });  
+//   } else {
+//     console.log('Service workers aren\'t supported in this browser.');
+//   }
+// });
+
+
+
+// function initialiseState(reg) {
+//   if (!(reg.showNotification)) {
+//     // console.log('Notifications aren\'t supported on service workers.');
+//   }
+
+//   if (Notification.permission === 'denied') {
+//     // console.log('The user has blocked notifications.');
+//     return;
+//   }
+
+//   if (!('PushManager' in window)) {
+//     // console.log('Push messaging isn\'t supported.');
+//     return;
+//   }
+
+//   navigator.serviceWorker.ready.then(function(reg) {
+//     reg.pushManager.subscribe({ userVisibleOnly: true })
+//       .then(function(subscription) {
+
+//         if (!subscription) {
+//           // console.log('Not yet subscribed to Push')
+//           return;
+//         }
+
+//         var endpoint = subscription.endpoint;
+
+//         console.log(endpoint);
+//         // updateStatus(endpoint,key,'init');
+//       })  
+//       .catch(function(err) {
+//         console.log('Error during subscribe()', err);
+//       });
+//   });  
+// }
+
+// function saveEndpoint(endpoint) {
+//   // if endpoint.startswith('https://android.googleapis.com/gcm/send'):
+//   //   endpointParts = endpoint.split('/')
+//   //   registrationId = endpointParts[len(endpointParts) - 1]
+
+
+//   fetch('/users', {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       name: 'Hubot',
+//       login: 'hubot',
+//     })
+//   }).then(function(res){ 
+//     return res.json(); 
+//   }).then(function(data){ 
+//     alert( JSON.stringify( data ) ) 
+//   });
+// }
+
+
 Notification.requestPermission();
 
 window.addEventListener('load', function() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(reg) {
-      // if (reg.installing)
-      //   console.log('Service worker installing');
+    if ('serviceWorker' in navigator) {
+        console.log('Service worker registration.');
 
-      // else if(reg.waiting)
-      //   console.log('Service worker installed');
-
-      // else if(reg.active)
-      //   console.log('Service worker active');
-
-      initialiseState(reg);
-    });  
-  } else {
-    console.log('Service workers aren\'t supported in this browser.');
-  }
+        navigator.serviceWorker.register('/notifications/sw.js').then(function(reg) {
+            console.log(reg);
+            initialiseState(reg);
+        });
+    } else {
+        console.log('Error: Service workers aren\'t supported in this browser.');
+    }
 });
 
-
-
 function initialiseState(reg) {
-  if (!(reg.showNotification)) {
-    // console.log('Notifications aren\'t supported on service workers.');
-  }
+    if (!(reg.showNotification)) {
+        console.log('Error: Notifications aren\'t supported on service workers.');
+    }
 
-  if (Notification.permission === 'denied') {
-    // console.log('The user has blocked notifications.');
-    return;
-  }
+    if (Notification.permission === 'denied') {
+        console.log('Error: The user has blocked notifications.');
+        return;
+    }
 
-  if (!('PushManager' in window)) {
-    // console.log('Push messaging isn\'t supported.');
-    return;
-  }
+    if (!('PushManager' in window)) {
+        console.log('Error: Push messaging isn\'t supported.');
+        return;
+    }
 
-  navigator.serviceWorker.ready.then(function(reg) {
-    reg.pushManager.subscribe({ userVisibleOnly: true })
-      .then(function(subscription) {
+    navigator.serviceWorker.ready.then(function(reg) {
+        reg.pushManager.subscribe({ userVisibleOnly: true })
+            .then(function(subscription) {
 
-        if (!subscription) {
-          // console.log('Not yet subscribed to Push')
-          return;
-        }
+                if (!subscription) {
+                    console.log('Error: Not yet subscribed to Push');
+                    return;
+                }
 
-        var endpoint = subscription.endpoint;
+                var endpoint = subscription.endpoint;
+                console.log(endpoint);
 
-        console.log(endpoint);
-        // updateStatus(endpoint,key,'init');
-      })  
-      .catch(function(err) {
-        console.log('Error during subscribe()', err);
-      });
-  });  
+                // saveEndpoint(endpoint);
+            }).catch(function(err) {
+                console.log('Error: Error during subscribe()', err);
+            });
+    });
 }
 
 function saveEndpoint(endpoint) {
-  // if endpoint.startswith('https://android.googleapis.com/gcm/send'):
-  //   endpointParts = endpoint.split('/')
-  //   registrationId = endpointParts[len(endpointParts) - 1]
+    var key = endpoint.split("/")[5];
 
-
-  fetch('/users', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'Hubot',
-      login: 'hubot',
-    })
-  }).then(function(res){ 
-    return res.json(); 
-  }).then(function(data){ 
-    alert( JSON.stringify( data ) ) 
-  });
+    if (key) {
+        fetch('/wp-admin/admin-ajax.php?action=saveEndpoint&endpoint=' + key)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (result) {
+                console.log(result);
+            })
+            .catch (function (error) {
+                console.log('Request failed', error);
+            });
+    }
 }
--- sw.js
+++ sw.js
@@ -1,68 +1,137 @@
-var _apiURL = '/latest.json',
-	_errorIconUrl = 'http://hostadvice.com/wp-content/uploads/2013/06/icon-large-features.png';
+// var _apiURL = '/latest.json',
+// 	_errorIconUrl = 'http://hostadvice.com/wp-content/uploads/2013/06/icon-large-features.png';
+
+// self.addEventListener('push', function(event) {
+//   event.waitUntil(
+//     fetch(_apiURL).then(function(response) {
+
+//       if (response.status !== 200) {
+//         // console.log('Looks like there was a problem. Status Code: ' + response.status);
+//         throw new Error();
+//       }
+
+//       return response.json().then(function(data) {
+//         if (data.error || !data.notification) {
+//           // console.error('The API returned an error.', data.error);
+//           throw new Error();
+//         }
+
+//         var title = data.notification.title;
+//         var message = data.notification.message;
+//         var icon = data.notification.icon;
+//         var notificationTag = data.notification.tag;
+
+//         return self.registration.showNotification(title, {
+//           body: message,
+//           icon: icon,
+//           tag: notificationTag
+//         });
+//       });
+//     }).catch(function(err) {
+//       // console.error('Unable to retrieve data', err);
+
+//       var title = 'An error occurred';
+//       var message = 'We were unable to get the information for this push message';
+//       var icon = _errorIconUrl;
+//       var notificationTag = 'notification-error';
+//       return self.registration.showNotification(title, {
+//           body: message,
+//           icon: icon,
+//           tag: notificationTag
+//         });
+//     })
+//   );
+// });
+
+// self.addEventListener('notificationclick', function(event) {
+//   // console.log('On notification click: ', event.notification.tag);
+
+//   event.notification.close();
+
+//   event.waitUntil(
+//     clients.matchAll({
+//       type: "window"
+//     })
+//     .then(function(clientList) {
+//       for (var i = 0; i < clientList.length; i++) {
+//         var client = clientList[i];
+
+//         if (client.url == '/' && 'focus' in client)
+//           return client.focus();
+//       }
+
+//       if (clients.openWindow)
+//         return clients.openWindow('/');
+
+//     })
+//   );
+// });
+
+
+var _apiURL = 'latest.json', //'/wp-admin/admin-ajax.php?action=latestNotification',
+    _errorIconUrl = 'error.png';
 
 self.addEventListener('push', function(event) {
-  event.waitUntil(
-    fetch(_apiURL).then(function(response) {
+    event.waitUntil(
+        fetch(_apiURL).then(function(response) {
 
-      if (response.status !== 200) {
-        // console.log('Looks like there was a problem. Status Code: ' + response.status);
-        throw new Error();
-      }
+            if (response.status !== 200) {
+                // console.log('Looks like there was a problem. Status Code: ' + response.status);
+                throw new Error();
+            }
 
-      return response.json().then(function(data) {
-        if (data.error || !data.notification) {
-          // console.error('The API returned an error.', data.error);
-          throw new Error();
-        }
+            return response.json().then(function(data) {
+                if (data.error || !data.notification) {
+                    // console.error('The API returned an error.', data.error);
+                    throw new Error();
+                }
 
-        var title = data.notification.title;
-        var message = data.notification.message;
-        var icon = data.notification.icon;
-        var notificationTag = data.notification.tag;
+                var title = data.notification.title,
+                    message = data.notification.message,
+                    icon = data.notification.icon,
+                    notificationTag = data.notification.tag;
 
-        return self.registration.showNotification(title, {
-          body: message,
-          icon: icon,
-          tag: notificationTag
-        });
-      });
-    }).catch(function(err) {
-      // console.error('Unable to retrieve data', err);
+                return self.registration.showNotification(title, {
+                    body: message,
+                    icon: icon,
+                    tag: notificationTag
+                });
+            });
+        }).catch(function(err) {
+            // console.error('Unable to retrieve data', err);
 
-      var title = 'An error occurred';
-      var message = 'We were unable to get the information for this push message';
-      var icon = _errorIconUrl;
-      var notificationTag = 'notification-error';
-      return self.registration.showNotification(title, {
-          body: message,
-          icon: icon,
-          tag: notificationTag
-        });
-    })
-  );
+            var title = 'An error occurred',
+                message = 'We were unable to get the information for this push message',
+                icon = _errorIconUrl,
+                notificationTag = 'notification-error';
+
+            return self.registration.showNotification(title, {
+                body: message,
+                icon: icon,
+                tag: notificationTag
+            });
+        })
+    );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  // console.log('On notification click: ', event.notification.tag);
+    // console.log('On notification click: ', event.notification.tag);
 
-  event.notification.close();
+    event.notification.close();
 
-  event.waitUntil(
-    clients.matchAll({
-      type: "window"
-    })
-    .then(function(clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        var client = clientList[i];
+    event.waitUntil(
+        clients.matchAll({
+                type: "window"
+            }).then(function(clientList) {
+                for (var i = 0; i < clientList.length; i++) {
+                    var client = clientList[i];
 
-        if (client.url == '/' && 'focus' in client)
-          return client.focus();
-      }
+                    if (client.url == '/' && 'focus' in client)
+                        return client.focus();
+                }
 
-      if (clients.openWindow)
-        return clients.openWindow('/');
-
-    })
-  );
+                if (clients.openWindow)
+                    return clients.openWindow('/');
+            })
+    );
 });
